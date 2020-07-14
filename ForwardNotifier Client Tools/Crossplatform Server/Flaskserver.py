be3e3baf7b3c38a3dba134748072b1c2593bb2d1
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2020-07-15 00:50:24 +0200

    new flask server

--- ForwardNotifier Client Tools/Crossplatform Server/Flaskserver.py
+++ ForwardNotifier Client Tools/Crossplatform Server/Flaskserver.py
@@ -7,21 +7,24 @@ import base64
 import requests
 import re
 import time
+import flask
+from flask import request, jsonify
 
 if platform.system() == "Windows":
     from PIL import Image  # convert to ico
     from win10toast import ToastNotifier
     toaster = ToastNotifier()
 
-port = 8000
-
 version = "1.0.1"
+port = 8000
+OSs = ["Windows", "Linux", "MacOS"]
 iconpath = {
     "Windows": "/temp/ForwardNotifierIcon",
     "Linux": "/tmp/ForwardNotifierIcon",
-    "Darwin": "/tmp/ForwardNotifierIcon", # macos
+    "Darwin": "/tmp/ForwardNotifierIcon",  # macos
     "MacOS": "/tmp/ForwardNotifierIcon"
 }
+
 
 tries = 0
 def checkforupadate():
@@ -59,9 +62,9 @@ def sendnotif(Title, Message, OS, icon=None):
         if icon:
             print("Theres an icon!")
             icon = base64.decodebytes(icon.encode("utf-8"))
-            open(iconpath[OS], "wb").write(icon) # send img to correct path
+            open(iconpath[OS], "wb").write(icon)  # send img to correct path
 
-        print("Sending notification:")
+        print("\nSending notification:")
         print("Title:", Title)
         print("Message:", Message)
 
@@ -70,12 +73,12 @@ def sendnotif(Title, Message, OS, icon=None):
                 # try:  # Try to decode
                 filename = iconpath[OS]
                 img = Image.open(filename)
-                img.save(iconpath[OS] + '.ico', format = 'ICO')
+                img.save(iconpath[OS] + '.ico', format='ICO')
                 toaster.show_toast(Title,
-                                    Message,
-                                    icon_path=iconpath[OS] + ".ico",
-                                    duration=5,
-                                    threaded=True)
+                                   Message,
+                                   icon_path=iconpath[OS] + ".ico",
+                                   duration=5,
+                                   threaded=True)
                 # except:  # icon not base64 aka ignore
                 #     toaster.show_toast(Title,
                 #                        Message,
@@ -83,9 +86,9 @@ def sendnotif(Title, Message, OS, icon=None):
                 #                        threaded=True)
             else:
                 toaster.show_toast(Title,
-                                Message,
-                                duration=5,
-                                threaded=True)
+                                   Message,
+                                   duration=5,
+                                   threaded=True)
         elif OS == "Linux":
             if icon:
                 subprocess.call(
@@ -93,23 +96,25 @@ def sendnotif(Title, Message, OS, icon=None):
             else:
                 subprocess.call(
                     ["notify-send", "-i", "applications-development", Title, Message])
-        elif OS == "Darwin" or OS == "MacOS": # macos
+        elif OS == "Darwin" or OS == "MacOS":  # macos
             if icon:
                 subprocess.call(["/usr/local/bin/terminal-notifier",
-                                "-sound", "pop", "-appIcon", iconpath[OS], "-title", Title, "-message", Message])
+                                 "-sound", "pop", "-appIcon", iconpath[OS], "-title", Title, "-message", Message])
             else:
                 subprocess.call(["/usr/local/bin/terminal-notifier",
-                    "-sound", "pop", "-title", Title, "-message", Message])
+                                 "-sound", "pop", "-title", Title, "-message", Message])
     except:
-        sendnotif("Error", "unknown error while sending notification", platform.system())
+        # sendnotif("Error", "unknown error while sending notification",
+        #           platform.system())
+        pass
 
 
 def checkbody(body):  # checking the body for a post request, wont be a problem since we send it
     try:
-        try:
-            body = json.loads(body)
-        except:
-            return [False, "Unable to parse json"]
+        # try:
+        #     body = json.loads(body)
+        # except:
+        #     return [False, "Unable to parse json"]
 
         if "Title" not in body:
             return [False, "No 'Title' in body"]
@@ -144,84 +149,34 @@ def checkbody(body):  # checking the body for a post request, wont be a problem 
         return [False, "unknown error"]
 
 
-class S(BaseHTTPRequestHandler):
-    def send_res(self, args, code=200, Success=True):
-
-        self.send_response(code)
-        self.send_header('Content-type', 'text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        out = {
-            "Success": Success,
-            "value": args
-        }
-        print("Sending: ", json.dumps(out).encode('utf-8'))
-        self.wfile.write(json.dumps(out).encode('utf-8'))
-
-    def do_GET(self):
-
-        print("\nPath:", self.path)
-        print(self.headers)
-
-        self.send_res(
-            "Send a Post with a title and a message in a json format")
-
-    def do_POST(self):
-        # <--- Gets the size of data
-        content_length = int(self.headers['Content-Length'])
-        # <--- Gets the data itself
-        post_data = self.rfile.read(content_length)
-        # print(json.loads(post_data.decode('utf-8')))
-        print("\nPath:", str(self.path),
-              "\nHeaders:\n" + str(self.headers))
-        print(content_length)
-        if content_length > 0:
-            try:
-                body = post_data.decode('utf-8')
-                print("Body:\n" + post_data.decode('utf-8'), "\n")
-            except UnicodeEncodeError as e:
-                print("ForwardNotifierReciver Error:", e)
-                sendnotif("ForwardNotifierReciver Error:",
-                          "invalid characters", platform.system())
-            if checkbody(body)[0] == True:  # all good
-
-                body = json.loads(body)
-                if "img" in  body:
-                    sendnotif(body["Title"], body["Message"],
-                              body["OS"], body["img"])  # sends the body
-                else:
-                    sendnotif(body["Title"], body["Message"],
-                              body["OS"])  # sends the body
-
-                self.send_res("Sent!")
-
-            else:  # Body is wrong
-
-                print(checkbody(body)[1])  # send the error
-                self.send_res(checkbody(body)[1], Success=False, code=400)
-
-        else:
-            self.send_res(
-                "POST request for {} . Please send a body".format(self.path), Success=False)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-def run(server_class=HTTPServer, handler_class=S, port=port):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print('Starting httpd on port', port, '...')
-    checkforupadate()
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print('\nStopping httpd...')
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': "ForwardNotifierServer, send a post w/ a 'Title', 'Message', 'OS' (Windows, MacOS, Linux), and optionally a base64 encoded image as 'img'"})
 
 
-if __name__ == '__main__':
-    from sys import argv
+@app.route('/', methods=['POST'])
+def notif():
+    body = request.json
+    print("\n", "="*20, "\n")
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
+    check = checkbody(body) # check the body
+
+    if not check[0]:  # body is wrong in some way
+        return jsonify({'message': check[1]})
+
+    if "img" in check: # theres an image
+        sendnotif(body["Title"], body["Message"],
+                  body["OS"], body["img"])
     else:
-        run()
+        sendnotif(body["Title"], body["Message"],
+                  body["OS"])
+
+    return jsonify({'message': "Success!"})
+
+
+app.run(port=port)
+checkforupadate()
